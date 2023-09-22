b417613d00b31b4199b767af007eb60d43b10f83
unknown <cristhian.mirandas@gmail.com>
2023-09-21 20:06:05 -0500

    Dart intro

---
+++ 01_dart_intro/02_variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  var variable = "hola";
+
+  //* Las constantes deben ser inicializadas al crearlas
+  //const pokemon;
+
+  //? Las variables sin valor toman el valor dynamic que pueden recibir cualquier tipo de valor sin importar el tipo
+  final variable2;
+
+  //? Podemos definir despues la variable
+  late final pokemon2;
+
+  pokemon2 = "otro pokemon";
+
+  //! No se puede reasignar
+  //pokemon = "otro pokemon";
+
+  //! No se puede reasignar
+  //variable2 = "otra cosa";
+
+  print(variable);
+  print(pokemon2);
+}
